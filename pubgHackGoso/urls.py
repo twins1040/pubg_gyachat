f3930d9b0afbcb6b1f4e45541b6688e6ccdd45b6
sweet_winter <twins1040@gmail.com>
2018-08-08 18:06:41 +0900

    /pubg/players/pc-na/shroud will return his player_id

--- pubgHackGoso/urls.py
+++ pubgHackGoso/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<playerName>/', views.searchPlayer, name='searchPlayer')
+    path('players/<shard>/<playerName>/', views.searchPlayer, name='searchPlayer')
 ]
--- pubgHackGoso/views.py
+++ pubgHackGoso/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from pubg_python import PUBG, Shard
+from pubg_python.exceptions import NotFoundError
+
 # Create your views here.
 def index(request):
     return HttpResponse("hello, victims! I will goso hack user")
 
-def searchPlayer(request, playerName):
-    response = "You're looking at the result %s."
-    return HttpResponse(response % playerName)
+def searchPlayer(request, shard, playerName):
+    shardEnum = ""
+    for s in Shard:
+        if s.value == shard:
+            shardEnum = s
+
+    api = PUBG('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0NmEwOGE2MC03MDc2LTAxMzYtOTNmMS0wNDk5NWRmYzQ5Y2EiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTMyMzMwMjAzLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImJhdHRsZXJhZGFyIn0.AE8Q2-3vwrpWrKlLS43ezcucpZN9Eq0rFG654q6PKmw', shardEnum)
+
+    try:
+        players = api.players().filter(player_names=[playerName])
+        for player in players:
+            player_id = player.id
+    except NotFoundError:
+        response = "Name error"
+    else:
+        response = ' '.join([playerName, "'s id is", player_id])
+
+    return HttpResponse(response)
 
